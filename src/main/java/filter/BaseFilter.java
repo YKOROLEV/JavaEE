9a3f91c7d000ea10b52c997991d8377ce0579e45
Ярослав Королев <1201576@gmail.com>
2019-12-05 14:32:03 +0300

    refactoring BaseFilter

--- src/main/java/filter/BaseFilter.java
+++ src/main/java/filter/BaseFilter.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpSession;
 
 public abstract class BaseFilter extends HttpFilter {
 
+    protected long getUserId(HttpSession session) {
+        return (long) session.getAttribute("user_id");
+    }
+
     protected boolean hasAuthorization(HttpSession session) {
         return session.getAttribute("user_id") != null
                 && session.getAttribute("user_role") != null;
-    }
-
-    protected long getUserId(HttpSession session) {
-        return (long) session.getAttribute("user_id");
     }
 
     protected boolean hasAdmin(HttpSession session) {
